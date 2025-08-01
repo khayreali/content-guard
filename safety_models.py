9cfa911274542b4c71c14f632e0c1b01403cb4ff
Khayre <khayreahmed@gmail.com>
2025-07-31 20:48:21 -0400

    updating to falcon vision model

--- safety_models.py
+++ safety_models.py
@@ -1,15 +1,13 @@
 import torch
 import warnings
 from PIL import Image
-from transformers import logging, pipeline, ViTForImageClassification, ViTFeatureExtractor
-
+from transformers import logging, pipeline, AutoProcessor, ShieldGemma2ForImageClassification
 
 logging.set_verbosity_error()
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+# Text Model
 classifier = pipeline("text-classification", model="patrickquick/BERTicelli")
-model = ViTForImageClassification.from_pretrained('jaranohaal/vit-base-violence-detection')
-feature_extractor = ViTFeatureExtractor.from_pretrained('jaranohaal/vit-base-violence-detection')
 
 def text_classifier(txt):
     classification = classifier(txt)
@@ -23,51 +21,16 @@ def text_classifier(txt):
     
     return output
 
-def image_classifier(img):
+def nsfw_classifier(img):
+    img = Image.open(img)
+    classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
+    result = classifier(img)[0]['label']
 
-    # Load an image
-    image = Image.open(img)
+    if result == 'normal':
+        output = "Text does no contain NSFW material."
+    elif result == 'nsfw':
+        output = 'Text contains NSFW material.'
 
-    # Preprocess the image
-    inputs = feature_extractor(images=image, return_tensors="pt")
+    return output
 
-    # Perform inference
-    with torch.no_grad():
-        outputs = model(**inputs)
-        logits = outputs.logits
-        predicted_class_idx = logits.argmax(-1).item()
-    prediction = ("Predicted class:", model.config.id2label[predicted_class_idx])
-
-    return prediction
-
-# pip install accelerate
-
-from transformers import AutoProcessor, ShieldGemma2ForImageClassification
-from PIL import Image
-import torch
-
-# --- 1. Define the policy and create the prompt ---
-# You must choose which policy you want to check for.
-policy = "No Explicit Content" 
-prompt = f"Does the image violate the following policy? {policy}"
-
-# --- Setup remains the same ---
-model_id = "google/shieldgemma-2-4b-it"
-image = Image.open('flowers.jpg') # Make sure this image should violate the policy above
-
-model = ShieldGemma2ForImageClassification.from_pretrained(model_id).eval()
-processor = AutoProcessor.from_pretrained(model_id)
-
-# --- 2. Pass both the prompt and the image to the processor ---
-model_inputs = processor(
-    text=[prompt], # Pass the prompt as text
-    images=[image], 
-    return_tensors="pt"
-)
-
-# --- Inference and output remain the same ---
-with torch.inference_mode():
-    scores = model(**model_inputs)
-
-print(f"Policy: {policy}")
-print(scores.probabilities)
+print(nsfw_classifier('flowers.jpg'))
